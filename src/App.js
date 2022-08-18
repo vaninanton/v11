216597e5cf94f1f21b773e43ce93d849514f65cc
Tony V <vaninanton@gmail.com>
2022-08-18 23:47:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Testcomponent from './testcomponent';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Inmotion V11</h1>
+      <Testcomponent></Testcomponent>
+    </div>
+  );
+}
+
+export default App;
